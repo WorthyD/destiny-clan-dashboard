7b106da760e347ec9cb0c9e21c9dc362b1fce72e
WorthyD <danieljworthy@gmail.com>
2022-09-06 05:58:58 -0500

    Updating config

--- projects/clan-dashboard/src/app/app.component.ts
+++ projects/clan-dashboard/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'clan-dashboard';
+  test = environment.apiKey;
 }
---
+++ set-env.ts
@@ -0,0 +1,17 @@
+var fs = require('fs');
+// Configure Angular `environment.ts` file path
+const targetPath = './projects/clan-dashboard/src/environments/secrets.ts';
+
+const envConfigFile = `export const secretKeys = {
+  apiKey: '${process.env.API_KEY}',
+};
+`;
+
+
+fs.writeFile(targetPath, envConfigFile, function (err) {
+  if (err) {
+      throw console.error(err);
+  } else {
+      console.log(`Angular environment.ts file generated correctly at ${targetPath} \n`);
+  }
+});
