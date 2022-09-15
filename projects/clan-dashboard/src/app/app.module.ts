dbdbae457cd8f655c067e35e94da67ff202eabb2
WorthyD <danieljworthy@gmail.com>
2022-09-15 07:45:53 -0500

    Updates

--- projects/clan-dashboard/src/app/app.module.ts
+++ projects/clan-dashboard/src/app/app.module.ts
@@ -5,10 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from '@core/core.module';
 
+
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, AppRoutingModule, CoreModule],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 })
 export class AppModule {}
--- projects/clan-dashboard/src/app/core/core.module.ts
+++ projects/clan-dashboard/src/app/core/core.module.ts
@@ -4,18 +4,28 @@ import { AppConfig } from './config/app-config';
 import { environment } from '../../environments/environment';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { ApiKeyInterceptor } from './interceptors/apikey.interceptor';
+import { StoreModule } from '@ngrx/store';
+import { coreReducers, coreEffects } from './core.state';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, HttpClientModule],
+  imports: [
+    CommonModule,
+    HttpClientModule,
+    StoreModule.forRoot(coreReducers),
+    EffectsModule.forRoot(coreEffects),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production })
+  ],
   providers: [
     { provide: AppConfig, useValue: environment },
     {
       provide: HTTP_INTERCEPTORS,
       useClass: ApiKeyInterceptor,
-      multi: true,
-    },
-  ],
+      multi: true
+    }
+  ]
 })
 export class CoreModule {
   constructor(
