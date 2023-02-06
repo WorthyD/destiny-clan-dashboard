cb4f622cfd11f15373ce2affe42449f5c0db4dac
WorthyD <danieljworthy@gmail.com>
2023-02-06 05:50:28 -0600

    adding back button to tables

--- projects/clan-dashboard/src/app/features/activities/activity-table/activity-table.module.ts
+++ projects/clan-dashboard/src/app/features/activities/activity-table/activity-table.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ActivityTableComponent } from './activity-table.component';
 import { TableViewComponent } from '@destiny/components/shared/table-view';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterModule } from '@angular/router';
 @NgModule({
   declarations: [ActivityTableComponent],
   exports: [ActivityTableComponent],
-  imports: [CommonModule, TableViewComponent]
+  imports: [CommonModule, MatButtonModule, MatIconModule, TableViewComponent, RouterModule ]
 })
 export class ActivityTableModule {}
