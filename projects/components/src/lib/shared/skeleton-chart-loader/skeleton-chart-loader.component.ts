7f7ff492556fc8ef1b8d7ee47f145905a6b1ddfd
WorthyD <danieljworthy@gmail.com>
2023-03-07 13:27:00 -0600

    Updates

--- projects/components/src/lib/shared/skeleton-chart-loader/skeleton-chart-loader.component.ts
+++ projects/components/src/lib/shared/skeleton-chart-loader/skeleton-chart-loader.component.ts
@@ -5,9 +5,48 @@ import { CommonModule } from '@angular/common';
   selector: 'lib-skeleton-chart-loader',
   standalone: true,
   imports: [CommonModule],
-  templateUrl: './skeleton-chart-loader.component.html',
-  styleUrls: ['./skeleton-chart-loader.component.scss']
-})
-export class SkeletonChartLoaderComponent {
+  styles: [
+    `
+      .skeleton-chart-loader {
+        height: 400px;
+        width: 100%;
+        display: flex;
+        align-items: flex-end;
+        justify-content: space-between;
 
-}
+        &__chart-item {
+          flex: 1 1 15%;
+          display: block;
+          position: relative;
+          //text-align: center;
+          //vertical-align: bottom;
+          max-width: 20%;
+          height: 100%;
+          margin: 0 0.9% 0 0.9%;
+
+          $i: 1;
+          $ref: 20%, 25%, 40%, 45%, 65%, 50%, 45%, 55%, 65%, 75%, 85%;
+
+          @for $i from 1 through length($ref) {
+            $c: nth($ref, $i);
+
+            &:nth-child(#{$i}) {
+              height: $c;
+            }
+          }
+        }
+      }
+    `
+  ],
+  template: `
+    <div class="skeleton-chart-loader">
+      <div
+        class="skeleton-chart-loader__chart-item skeleton-item"
+        *ngFor="let x of ['', '', '', '', '', '', '', '', '', '', '']"
+      ></div>
+    </div>
+  `,
+  //templateUrl: './skeleton-chart-loader.component.html',
+  //styleUrls: ['./skeleton-chart-loader.component.scss']
+})
+export class SkeletonChartLoaderComponent {}
