0252ba0b558ec82170a84a1e7963e21e3ee61a08
WorthyD <danieljworthy@gmail.com>
2024-04-02 05:27:23 -0500

    chore: deleting old files

--- apps/destiny-clan-dashboard/src/app/core/definition-services/milestone-definition.service.ts
+++
@@ -1,8 +0,0 @@
-// import { Injectable } from '@angular/core';
-// import { DestinyDefinitionsMilestonesDestinyMilestoneDefinition } from 'bungie-api-angular';
-// import { BaseDefinitionService } from './base-definition.service';
-
-// @Injectable({
-//   providedIn: 'root'
-// })
-// export class MilestoneDefinitionService extends BaseDefinitionService<DestinyDefinitionsMilestonesDestinyMilestoneDefinition> {}
--- apps/destiny-clan-dashboard/src/app/core/definition-services/record-definition.service.ts
+++
@@ -1,8 +0,0 @@
-// import { Injectable } from '@angular/core';
-// import { DestinyDefinitionsRecordsDestinyRecordDefinition } from 'bungie-api-angular';
-// import { BaseDefinitionService } from './base-definition.service';
-
-// @Injectable({
-//   providedIn: 'root'
-// })
-// export class RecordDefinitionService extends BaseDefinitionService<DestinyDefinitionsRecordsDestinyRecordDefinition> {}
