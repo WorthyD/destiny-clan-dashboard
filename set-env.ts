aa225ed2f0234af7305e1adfec55f2af2554cc57
WorthyD <danieljworthy@gmail.com>
2024-04-21 20:12:49 -0500

    chore: fixing environment config

--- set-env.ts
+++ set-env.ts
@@ -1,17 +1,16 @@
 var fs = require('fs');
 // Configure Angular `environment.ts` file path
-const targetPath = './projects/clan-dashboard/src/environments/secrets.ts';
+const targetPath = './apps/destiny-clan-dashboard/src/environments/secrets.ts';
 
 const envConfigFile = `export const secretKeys = {
   apiKey: '${process.env['API_KEY']}',
 };
 `;
 
-
-fs.writeFile(targetPath, envConfigFile, function (err) {
+fs.writeFile(targetPath, envConfigFile, function (err: any) {
   if (err) {
-      throw console.error(err);
+    throw console.error(err);
   } else {
-      console.log(`Angular environment.ts file generated correctly at ${targetPath} \n`);
+    console.log(`Angular environment.ts file generated correctly at ${targetPath} \n`);
   }
 });
