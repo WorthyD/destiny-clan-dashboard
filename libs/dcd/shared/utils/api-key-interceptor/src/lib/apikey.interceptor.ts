7f3c0f7d9a3433c6c373967c635d4d3fc62d955b
WorthyD <danieljworthy@gmail.com>
2024-05-31 17:45:30 -0500

    chore: update angulare

--- libs/dcd/shared/utils/api-key-interceptor/src/lib/apikey.interceptor.ts
+++ libs/dcd/shared/utils/api-key-interceptor/src/lib/apikey.interceptor.ts
@@ -1,27 +1,41 @@
-import { Inject, Injectable } from '@angular/core';
+import { Inject, Injectable, inject } from '@angular/core';
 import {
   HttpEvent,
   HttpInterceptor,
   HttpHandler,
-  HttpRequest
+  HttpRequest,
+  HttpInterceptorFn,
+  HttpHandlerFn
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
 //import { AppConfig } from '@core/config/app-config';
 import { AppConfigService } from '@dcd/shared/utils/app-config';
 
-@Injectable()
-export class ApiKeyInterceptor implements HttpInterceptor {
-  constructor(private appConfig: AppConfigService) {}
+// @Injectable()
+// export class ApiKeyInterceptor implements HttpInterceptor {
+//   constructor(private appConfig: AppConfigService) {}
 
-  intercept(
-    req: HttpRequest<any>,
-    next: HttpHandler
-  ): Observable<HttpEvent<any>> {
-    // add the header to the cloned request
-    const authReq = req.clone({
-      headers: req.headers.set('x-api-key', this.appConfig.config.apiKey)
-    });
+//   intercept(
+//     req: HttpRequest<any>,
+//     next: HttpHandler
+//   ): Observable<HttpEvent<any>> {
+//     console.log('apikey', this.appConfig.config.apiKey);
+//     // add the header to the cloned request
+//     const authReq = req.clone({
+//       headers: req.headers.set('x-api-key', this.appConfig.config.apiKey)
+//     });
 
-    return next.handle(authReq);
-  }
-}
+//     return next.handle(authReq);
+//   }
+// }
+
+export const ApiKeyInterceptor: HttpInterceptorFn = (req: HttpRequest<unknown>, next: HttpHandlerFn) => {
+  const appConfig = inject(AppConfigService);
+  console.log('apikey', appConfig.config.apiKey);
+  // add the header to the cloned request
+  const authReq = req.clone({
+    headers: req.headers.set('x-api-key', appConfig.config.apiKey)
+  });
+
+  return next(authReq);
+};
