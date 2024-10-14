8d6874f6308a644c1b772d81d02d296f7b4f64a1
WorthyD <danieljworthy@gmail.com>
2024-10-14 05:17:32 -0500

    adding signal store and a bunch of other stuff

--- libs/dcd/shared/ui/member/src/index.ts
+++ libs/dcd/shared/ui/member/src/index.ts
@@ -1,6 +1,7 @@
 export * from './lib/bungie-info/bungie-info.component';
 export * from './lib/character-card/character-card.component';
 export * from './lib/member-type-icon/member-type-icon.component';
+export * from './lib/member-overview/member-overview.component';
 export * from './lib/player-seals/player-seal.component';
 export * from './lib/player-seals/member-profile.converter';
 export * from './lib/player-seals/player-seals.component';
